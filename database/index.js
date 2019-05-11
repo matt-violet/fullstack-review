f8f4791f14265421849b72405c5236971efcb94c
Matt Violet <mattviolet@Matts-MacBook-Pro.local>
2019-05-11 16:04:15 -0700

    Save repos to mongo db (including duplicates)

--- database/index.js
+++ database/index.js
@@ -12,29 +12,41 @@ db.once('open', function() {
 
 // define shape of documents (instances of model)
 let repoSchema = mongoose.Schema({
-  id: Number,
-  owner: Object,  // includes login username and repos url
-  description: String,
-  stargazers_count: Number 
+  userID: Number,
+  username: String,  
+  userPage: String,
+  userReposPage: String,
+  stargazersCount: Number
 });
 
-// makes a model/collection of schema
+// model class with which we construct documents (instances of model)
 let Repo = mongoose.model('Repo', repoSchema);
 
+// create collection to store repos
+db.createCollection("repos", { capped: false,
+  autoIndexId: true,
+  }
+)
 
-// save a repo or repos to the MongoDB
-// needs to prevent saving duplicates
+// save a repo or repos to the MongoDB and prevent duplicates
 let save = (repoData, callback) => {
   var parsedRepoData = JSON.parse(repoData);
-  console.log(parsedRepoData);
-  // var userID = JSON.parse(repoData).id
-
-  Repo.create(repoData, function (err, result) {
-    if (err) {
-      return callback(err);
-    }
-    callback(result);
-  });
+  for (var i = 0; i < parsedRepoData.length; i++) {
+    var repo = new Repo({
+      userID: parsedRepoData[i].id,
+      username: parsedRepoData[i].owner.login,  
+      userPage: parsedRepoData[i].owner.url,
+      userReposPage: parsedRepoData[i].owner.repos_url,
+      stargazersCount: parsedRepoData[i].stargazers_count
+    })
+    repo.save(function (err, data) {
+      if (err) {
+        console.log('Unable to save repo to database');
+        return;
+      }
+      console.log('Successfully saved repo to database!')
+    })
+  }
 }
 
 
